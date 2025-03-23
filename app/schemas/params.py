bbc6c821dc9269138f7d869a717768b6f98b852b
bissaliev <bissaliev21@gmail.com>
2025-03-23 11:18:17 +0400

    Рефакторинг: В schemas/params.py вынес общие поля в отдельный класс

--- app/schemas/params.py
+++ app/schemas/params.py
@@ -3,16 +3,16 @@ from datetime import date
 from pydantic import BaseModel
 
 
-class DynamicParams(BaseModel):
+class TradingParams(BaseModel):
     oil_id: str | None = None
     delivery_type_id: str | None = None
     delivery_basis_id: str | None = None
+
+
+class DynamicParams(TradingParams):
     start_date: date | None = None
     end_date: date | None = None
 
 
-class LastParams(BaseModel):
-    oil_id: str | None = None
-    delivery_type_id: str | None = None
-    delivery_basis_id: str | None = None
+class LastParams(TradingParams):
     limit: int = 10
