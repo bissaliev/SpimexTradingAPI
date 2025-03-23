36d6e69eb6cffc157443bc82210eb3efc8ed2e41
bissaliev <bissaliev21@gmail.com>
2025-03-23 11:19:55 +0400

    Создал зависимость TradingServiceDepends для сервиса TradingService

---
+++ app/api/dependencies.py
@@ -0,0 +1,14 @@
+from typing import Annotated
+
+from fastapi import Depends
+from services.tradings import TradingService
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from database.database import get_db
+
+
+def trading_service(session: Annotated[AsyncSession, Depends(get_db)]):
+    return TradingService(session)
+
+
+TradingServiceDepends = Annotated[TradingService, Depends(trading_service)]
