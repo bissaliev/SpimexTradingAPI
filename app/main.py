ca58a2afd1651046514b486cd914713f8e942e3a
bissaliev <bissaliev21@gmail.com>
2025-03-24 06:28:36 +0400

    Добавил функцию lifespan для инициализации redis-клиента

--- app/main.py
+++ app/main.py
@@ -1,8 +1,19 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from api.routers.tradings import router as trading_router
+from utils.redis_client import init_redis
 
-app = FastAPI(title="Spimex Trading API")
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    redis_client = await init_redis()
+    yield
+    await redis_client.close()
+
+
+app = FastAPI(title="Spimex Trading API", lifespan=lifespan)
 
 
 app.include_router(trading_router, prefix="/trading", tags=["Trading"])
