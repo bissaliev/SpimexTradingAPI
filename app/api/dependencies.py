b16f8f3d6c63fd14947e4bc2b4e77a8510e0a103
bissaliev <bissaliev21@gmail.com>
2025-03-27 07:51:22 +0400

    Рефакторинг: аннотации и докстринги в файле app/api/dependencies.py

--- app/api/dependencies.py
+++ app/api/dependencies.py
@@ -7,7 +7,13 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from database.database import get_db
 
 
-def trading_service(session: Annotated[AsyncSession, Depends(get_db)]):
+def trading_service(session: Annotated[AsyncSession, Depends(get_db)]) -> TradingService:
+    """
+    Функция для создания экземпляра TradingService.
+
+    :param session: Асинхронная сессия базы данных, полученная через Depends(get_db).
+    :return: Экземпляр TradingService, использующий переданную сессию.
+    """
     return TradingService(session)
 
 
