bfe952552c0aa5e09f06102e6581989f01d962ae
bissaliev <bissaliev21@gmail.com>
2025-03-27 07:52:31 +0400

    Рефакторинг: аннотации и докстринги в файле app/utils/redis_client.py

--- app/utils/redis_client.py
+++ app/utils/redis_client.py
@@ -7,20 +7,35 @@ from fastapi_cache.backends.redis import RedisBackend
 from configs.config import settings
 
 
-async def get_redis():
+async def get_redis() -> aioredis.Redis:
+    """
+    Создает подключение к Redis.
+
+    :return: Экземпляр асинхронного клиента Redis.
+    """
     host = settings.REDIS_HOST
     port = settings.REDIS_PORT
     redis = aioredis.from_url(f"redis://{host}:{port}", encoding="utf8", decode_responses=True)
     return redis
 
 
-async def init_redis():
+async def init_redis() -> aioredis.Redis:
+    """
+    Инициализирует кэш FastAPI с использованием Redis.
+
+    :return: Экземпляр асинхронного клиента Redis.
+    """
     redis_client = await get_redis()
     FastAPICache.init(RedisBackend(redis_client), prefix=settings.CACHE_PREFIX)
     return redis_client
 
 
 async def get_expiries() -> int:
+    """
+    Рассчитывает время (в секундах) до ближайшего сброса кэша в 14:11.
+
+    :return: Количество секунд до следующего сброса.
+    """
     now = datetime.now()
     reset_time = now.replace(hour=14, minute=11, second=0, microsecond=0)
     if now > reset_time:
