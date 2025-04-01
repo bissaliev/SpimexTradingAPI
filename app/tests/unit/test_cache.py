6f8ac6e354695198e956189bd73960b2563cf8f3
bissaliev <bissaliev21@gmail.com>
2025-04-01 15:48:59 +0400

    Добавил юнит-тесты для кеширования

---
+++ app/tests/unit/test_cache.py
@@ -0,0 +1,65 @@
+from datetime import datetime, timedelta
+from unittest.mock import Mock, patch
+
+import pytest
+from services.tradings import TradingService
+
+from utils.redis_client import get_expiries
+
+
+@pytest.mark.parametrize(
+    "return_value, expected",
+    (
+        (datetime(2025, 3, 31, 13, 0, 0), (14 * 3600 + 11 * 60) - (13 * 3600)),
+        (datetime(2025, 3, 31, 15, 0, 0), 24 * 3600 - (15 * 3600) + (14 * 3600 + 11 * 60)),
+        (datetime(2025, 3, 31, 14, 11, 1), 86399),
+    ),
+)
+@patch("utils.redis_client.datetime")
+async def test_get_expiries_return_correct_time(mock_datetime, return_value, expected):
+    """Функция возвращает корректное время в секундах для кеша"""
+    mock_datetime.now.return_value = return_value
+    result = get_expiries()
+    assert result == expected
+
+
+@pytest.mark.usefixtures("test_cache")
+class TestCacheUnit:
+    """Тестируем работу кеша для TradingService"""
+
+    @pytest.mark.parametrize("method", ("get_last_dates", "filter"))
+    async def test_work_cache(self, method, test_cache, mock_session, fixtures):
+        """
+        Тестируем что кеш сохраняется с корректным временем жизни
+        и отдает данные при следующих запросах из кеша до 14.11
+        """
+        mock_result = Mock()
+        mock_result.all.return_value = fixtures
+        mock_session.scalars.return_value = mock_result
+        trading_service = TradingService(mock_session)
+        await getattr(trading_service, method)()
+
+        keys = [key async for key in test_cache.scan_iter("test-cache:*")]
+        assert len(keys) == 1
+
+        # Текущее время
+        now = datetime.now()
+        # Время сброса кеша сегодня в 14:11
+        reset_time = now.replace(hour=14, minute=11, second=0, microsecond=0)
+        # Если текущее время позже 14:11, используем завтрашний день
+        if now > reset_time:
+            reset_time = reset_time + timedelta(days=1)
+
+        # Ожидаемое TTL в секундах
+        expected_ttl = int((reset_time - now).total_seconds())
+        tolerance = 10  # Допустимая погрешность в секундах
+        ttl = await test_cache.ttl(keys[0])
+        assert ttl > 0, f"Ключ {keys[0]} не имеет времени жизни"
+        assert abs(ttl - expected_ttl) <= tolerance, (
+            f"TTL ключа {keys[0]} ({ttl}) не соответствует ожидаемому ({expected_ttl})"
+        )
+
+        # Проверяем что второй раз данные забираются из кеша
+        await getattr(trading_service, method)()
+        assert mock_result.all.call_count == 1
+        assert mock_session.scalars.call_count == 1
