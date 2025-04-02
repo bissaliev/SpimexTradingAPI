b96d26542fc98ade4ff5f43dae87bfebe153354d
bissaliev <bissaliev21@gmail.com>
2025-04-02 11:53:06 +0400

    В integration/test_api.py вынес фикстуры в файл conftest.py

--- app/tests/integration/test_api.py
+++ app/tests/integration/test_api.py
@@ -2,39 +2,21 @@ import operator
 from datetime import date
 
 import pytest
-import pytest_asyncio
-from services.tradings import TradingService
-from sqlalchemy import insert
-
-from api.dependencies import trading_service
-from database.models import SpimexTradingResults
-
-
-@pytest_asyncio.fixture
-async def load_data_in_db(session, fixtures):
-    await session.execute(insert(SpimexTradingResults), fixtures)
-    await session.commit()
 
 
 @pytest.mark.usefixtures("test_cache", "async_db_engine", "load_data_in_db")
 class TestEndpoints:
-    async def test_get_last_trading_dates(self, app, async_client, session):
-        new_trading_service = TradingService(session)
-        app.dependency_overrides[trading_service] = lambda: new_trading_service
+    async def test_get_last_trading_dates(self, async_client):
         response = await async_client.get("/trading/last_trading_dates")
         assert response.status_code == 200
         assert len(response.json()["dates"]) == 3
 
-    async def test_get_trading_results_endpoint(self, app, async_client, session):
-        new_trading_service = TradingService(session)
-        app.dependency_overrides[trading_service] = lambda: new_trading_service
+    async def test_get_trading_results_endpoint(self, async_client):
         response = await async_client.get("/trading/trading_results")
         assert response.status_code == 200
         assert len(response.json()) == 3
 
-    async def test_dynamics_endpoint(self, app, async_client, session):
-        new_trading_service = TradingService(session)
-        app.dependency_overrides[trading_service] = lambda: new_trading_service
+    async def test_dynamics_endpoint(self, async_client):
         response = await async_client.get("/trading/dynamics")
         assert response.status_code == 200
         assert len(response.json()) == 3
@@ -43,15 +25,9 @@ class TestEndpoints:
         "field",
         ("oil_id", "delivery_type_id", "delivery_basis_id"),
     )
-    async def test_trading_results_endpoint_filter(self, async_client, session, fixtures, field, app):
-        new_trading_service = TradingService(session)
-        app.dependency_overrides[trading_service] = lambda: new_trading_service
-        limit = 10
-        offset = 0
-        filter_value = fixtures[0][field]
-        response = await async_client.get(
-            f"/trading/trading_results?{field}={filter_value}&limit={limit}&offset={offset}"
-        )
+    async def test_trading_results_endpoint_filter(self, async_client, tradings_list, field):
+        filter_value = tradings_list[0][field]
+        response = await async_client.get(f"/trading/trading_results?{field}={filter_value}")
         assert response.status_code == 200
         assert len(response.json()) == 1
         assert response.json()[0][field] == filter_value
@@ -60,10 +36,8 @@ class TestEndpoints:
         "field",
         ("oil_id", "delivery_type_id", "delivery_basis_id"),
     )
-    async def test_dynamics_endpoints_filters(self, async_client, session, field, fixtures, app):
-        new_trading_service = TradingService(session)
-        app.dependency_overrides[trading_service] = lambda: new_trading_service
-        obj = fixtures[0]
+    async def test_dynamics_endpoints_filters(self, async_client, field, tradings_list):
+        obj = tradings_list[0]
         value = obj[field]
         response = await async_client.get(f"trading/dynamics?{field}={value}")
         assert response.status_code == 200
@@ -79,12 +53,10 @@ class TestEndpoints:
             ("end_date", operator.le),
         ),
     )
-    async def test_method_filter_by_date(self, async_client, session, app, field, operator, fixtures):
-        new_trading_service = TradingService(session)
-        app.dependency_overrides[trading_service] = lambda: new_trading_service
-        obj = fixtures[0]
+    async def test_method_filter_by_date(self, async_client, field, operator, tradings_list):
+        obj = tradings_list[0]
         expected = sorted(
-            [i for i in fixtures if operator(i["date"], obj["date"])], key=lambda x: x["date"], reverse=True
+            [i for i in tradings_list if operator(i["date"], obj["date"])], key=lambda x: x["date"], reverse=True
         )
         response = await async_client.get(f"trading/dynamics?{field}={obj['date']}")
         assert response.status_code == 200
@@ -95,9 +67,7 @@ class TestEndpoints:
         "field",
         ("oil_id", "delivery_type_id", "delivery_basis_id"),
     )
-    async def test_trading_results_endpoint_filter_with_invalid_params(self, async_client, session, field, app):
-        new_trading_service = TradingService(session)
-        app.dependency_overrides[trading_service] = lambda: new_trading_service
+    async def test_trading_results_endpoint_filter_with_invalid_params(self, async_client, field):
         filter_value = ""
         response = await async_client.get(f"/trading/trading_results?{field}={filter_value}")
         assert response.status_code == 422
@@ -107,10 +77,8 @@ class TestEndpoints:
         (("oil_id", "X101"), ("delivery_type_id", "X"), ("delivery_basis_id", "NOT")),
     )
     async def test_trading_results_endpoint_filter_with_non_existent_data(
-        self, async_client, session, field, non_existent_data, app
+        self, async_client, field, non_existent_data
     ):
-        new_trading_service = TradingService(session)
-        app.dependency_overrides[trading_service] = lambda: new_trading_service
         response = await async_client.get(f"/trading/trading_results?{field}={non_existent_data}")
         assert response.status_code == 200
         assert len(response.json()) == 0
