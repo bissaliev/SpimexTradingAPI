d6f4365c58e9b094a26f61689e89c614f9700659
bissaliev <bissaliev21@gmail.com>
2025-04-02 15:38:42 +0400

    Рефакторинг файла для тестов tests/unit/test_cache.py(Докстринги и аннотации)

--- app/tests/unit/test_cache.py
+++ app/tests/unit/test_cache.py
@@ -1,7 +1,9 @@
 from datetime import datetime, timedelta
-from unittest.mock import Mock, patch
+from typing import Any
+from unittest.mock import AsyncMock, Mock, patch
 
 import pytest
+import redis.asyncio as aioredis
 from services.tradings import TradingService
 
 from utils.redis_client import get_expiries
@@ -16,30 +18,36 @@ from utils.redis_client import get_expiries
     ),
 )
 @patch("utils.redis_client.datetime")
-async def test_get_expiries_return_correct_time(mock_datetime, return_value, expected):
-    """Функция возвращает корректное время в секундах для кеша"""
+async def test_get_expiries_return_correct_time(mock_datetime: datetime, return_value: int, expected: int):
+    """Тест что функция `get_expiries` возвращает корректное время в секундах для кеша"""
     mock_datetime.now.return_value = return_value
     result = get_expiries()
     assert result == expected
 
 
-@pytest.mark.usefixtures("test_cache")
+@pytest.mark.usefixtures("test_redis_cache")
 class TestCacheUnit:
     """Тестируем работу кеша для TradingService"""
 
     @pytest.mark.parametrize("method", ("get_last_dates", "filter"))
-    async def test_work_cache(self, method, test_cache, mock_session, tradings_list):
+    async def test_working_redis_cache_trading_service(
+        self,
+        method: str,
+        test_redis_cache: aioredis.Redis,
+        mock_session: AsyncMock,
+        trading_data: list[dict[str, Any]],
+    ):
         """
         Тестируем что кеш сохраняется с корректным временем жизни
         и отдает данные при следующих запросах из кеша до 14.11
         """
         mock_result = Mock()
-        mock_result.all.return_value = tradings_list
+        mock_result.all.return_value = trading_data
         mock_session.scalars.return_value = mock_result
         trading_service = TradingService(mock_session)
         await getattr(trading_service, method)()
 
-        keys = [key async for key in test_cache.scan_iter("test-cache:*")]
+        keys = [key async for key in test_redis_cache.scan_iter("test-cache:*")]
         assert len(keys) == 1
 
         # Текущее время
@@ -53,7 +61,7 @@ class TestCacheUnit:
         # Ожидаемое TTL в секундах
         expected_ttl = int((reset_time - now).total_seconds())
         tolerance = 10  # Допустимая погрешность в секундах
-        ttl = await test_cache.ttl(keys[0])
+        ttl = await test_redis_cache.ttl(keys[0])
         assert ttl > 0, f"Ключ {keys[0]} не имеет времени жизни"
         assert abs(ttl - expected_ttl) <= tolerance, (
             f"TTL ключа {keys[0]} ({ttl}) не соответствует ожидаемому ({expected_ttl})"
